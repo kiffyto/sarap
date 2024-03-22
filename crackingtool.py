2e191a5351f60980c9ef5e1f40a4eea9ca26f317
Niño Infiesto <163622869+kiffyto@users.noreply.github.com>
2024-03-22 23:41:45 +0800

    Create crackingtool.py

---
+++ crackingtool.py
@@ -0,0 +1,6 @@
+import os
+os.system('clear')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('clear')
+os.system('git pull')
+import kiffy
