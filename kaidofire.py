c7f43f789543d34423e94d2b1344eed2fac25af1
Niño Infiesto <163622869+kiffyto@users.noreply.github.com>
2024-04-25 05:30:24 +0800

    Update kaidofire.py

--- kaidofire.py
+++ kaidofire.py
@@ -1,6 +1,1 @@
-import os
-os.system('clear')
-os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-os.system('clear')
-os.system('git pull')
 import kaidofire.py
