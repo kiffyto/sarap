717b916a3dd1b54f442678bc5b0921af7905656b
Niño Infiesto <163622869+kiffyto@users.noreply.github.com>
2024-04-25 05:29:28 +0800

    Update and rename crackingtool.py to kaidofire.py

--- kaidofire.py
+++ kaidofire.py
@@ -3,4 +3,4 @@ os.system('clear')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 os.system('clear')
 os.system('git pull')
-import kaido.py
+import kaidofire.py
